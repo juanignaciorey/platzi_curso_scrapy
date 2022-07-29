2cf9e4899162c29aa1d4b1e4e30fb5715de4479a
Puni <juanignaciorey92@gmail.com>
2022-07-28 21:01:59 -0300

    add READ.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# Platzi inteligence agency website
+
+_Es un proyecto de practica con Scrapy que nos permite crawlear los documentos públicos de la CIA_
+
+### Instalación 🔧
+
+```
+git clone https://github.com/juanignaciorey/platzi_curso_scrapy.git
+```
+
+## Carpetas 📋
+
+```
+.
+├── cia # Files de Scrapy Spider
+│ ├── .py # Spyder
+│ ├── .json # Output
+│ └── .JS # Fetch to json
+├── src # Source files
+├── LICENSE
+└── README.md
+```
+
+## Construido con 🛠️
+
+_Para crawlear y exportar a json_
+
+- [Scrapy](https://scrapy.org/) -
+- [Maven](https://maven.apache.org/) - Manejador de dependencias
+- [ROME](https://rometools.github.io/rome/) - Usado para generar RSS
---
+++ cia/cia.py
@@ -0,0 +1,46 @@
+from gc import callbacks
+import scrapy
+
+# XPath
+
+# links = //a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href
+# title = //h1[@class="documentFirstHeading"]/text()
+# paragraph = //div[@class="field-item even"]//p[not(@class)]/text()
+
+
+class SpiderCIA(scrapy.Spider):
+    name = 'cia'
+    start_urls = [
+        'https://www.cia.gov/readingroom/historical-collections'
+    ]
+
+    custom_settings = {
+        'FEEDS': {
+            'cia.json': {
+                'format': 'json',
+                'encoding': 'utf-8',
+                'indent': 4,
+            }
+        },
+    }
+
+    def parse(self, response):
+        links_desclassified = response.xpath(
+            '//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
+
+        for link in links_desclassified:
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
+
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+
+        title = response.xpath(
+            '//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath(
+            '//div[@class="field-item even"]//p[not(@class)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
